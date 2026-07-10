0916049b65fb38cff5d8eeffaefe9147245e592a
Ei-ji Nakama <nakama@ki.rim.or.jp>
2026-07-10 20:37:29 +0900

    tweaks

--- src/RhpcMPI.c
+++ src/RhpcMPI.c
@@ -162,7 +162,7 @@ static int cheatMPImaster(void)
   /* mpiexec */
   fakemastercmd(cmd,sizeof(cmd)-1, pipename);
   ZeroMemory(&fakemaster_si, sizeof(fakemaster_si))
-  fakemaster_si.cb = sizeof(si);
+  fakemaster_si.cb = sizeof(fakemaster_si);
   fakemaster_si.dwFlags = STARTF_USESHOWWINDOW;
   fakemaster_si.wShowWindow = SW_SHOWMINIMIZED;
 
